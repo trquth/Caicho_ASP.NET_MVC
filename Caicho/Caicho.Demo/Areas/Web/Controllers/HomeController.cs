009eba5cb7b14d55c18aea642732f550e64726df
dat.tran <dat.tran@saigontechnology.vn>
2017-05-23 17:37:00 +0700

    Check file exists on Server

--- Caicho/Caicho.Demo/Areas/Web/Controllers/HomeController.cs
+++ Caicho/Caicho.Demo/Areas/Web/Controllers/HomeController.cs
@@ -11,8 +11,26 @@ namespace Caicho.Demo.Areas.Web.Controllers
         // GET: Web/Home
         public ActionResult Index()
         {
+            bool test = CheckFileExitOnServer("~/build", "index.html");
+            if (test)
+            {
+                return new FilePathResult("~/build/index.html", "text/html");
+            }
+            //Change by another page such as Error page
             return new FilePathResult("~/build/index.html", "text/html");
             //return View();
+        }
+
+        private bool CheckFileExitOnServer(string path, string fileName)
+        {
+            bool exists = false;
+            string relativePath = String.Format("{0}/{1}", path, fileName);
+            string absolutePath = HttpContext.Server.MapPath(relativePath);
+            if (System.IO.File.Exists(absolutePath))
+            {
+                exists = true;
+            }
+            return exists;
         }
     }
 }
